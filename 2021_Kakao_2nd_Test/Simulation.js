e4455cfe614b43bbc92dd9c73afa1de19d7fcb86
alsrl8259 <alsrl8259@gmail.com>
2021-09-24 15:39:06 +0900

    Random Move

--- 2021_Kakao_2nd_Test/Simulation.js
+++ 2021_Kakao_2nd_Test/Simulation.js
@@ -43,81 +43,64 @@ let fail = 0;
 // console.log(await getLocations(auth_key));
 
 while(1){
-    // let truckInfo = await getTrucks(auth_key);
-    // setTruck(truck, truckInfo);
-    // let tmap = [];
-    // for(let i=0; i<5; i++){
-    //     let tmp = [];
-    //     for(let j=0; j<5; j++){
-    //         tmp.push(-1);
-    //     }
-    //     tmap.push(tmp);
-    // }
-    // for(let i=0; i<5; i++){
-    //     let r = truck[i].r;
-    //     let c = truck[i].c;
-    //     tmap[r][c] = i;
-    // }
-    // console.log(tmap);
-    // let locationInfo = await getLocations(auth_key);
-    // setMap(map, locationInfo);
-    // console.log(map);
+    let locationInfo = await getLocations(auth_key);
+    setMap(map, locationInfo);
     let orderList = [];
-    // for(let i=0; i<5; i++){
-    //     let r = truck[i].r;
-    //     let c = truck[i].c;
-    //     let d = truck[i].d;
-    //     let cnt = truck[i].cnt;
-    //     let bick = truck[i].bick;
-    //     let orderCnt = 0;
-    //     let order = [];
-    //     let result = {};
-    //     while(orderCnt<10){
-    //         if((orderCnt+cnt)%7 === 0){
-    //             d = (d+1)%4;
-    //         }
-    //         let nr = r + dr[d];
-    //         let nc = c + dc[d];
-    //         if(nr<0||nc<0||nr>=5||nc>=5){
-    //             d = (d+2)%4;
-    //             continue;
-    //         }
-    //         if(map[r][c]>3){
-    //             let want = map[r][c] - 4;
-    //             while(want!==0&&orderCnt<10&&bick<20){
-    //                 want--;
-    //                 orderCnt++;
-    //                 bick++;
-    //                 order.push(5);
-    //                 map[r][c]--;
-    //             }
-    //         }
-    //         else if(map[r][c]<2){
-    //             let want = 4 - map[r][c];
-    //             while(want!==0&&orderCnt<10&&bick>0){
-    //                 want--;
-    //                 orderCnt++;
-    //                 bick--;
-    //                 order.push(6);
-    //                 map[r][c]++;
-    //             }
-    //         }
-    //         if(orderCnt>=10)    break;
-    //         r = nr;
-    //         c = nc;
-    //         go[r][c]++;
-    //         order.push(d+1);
-    //         orderCnt++;
-    //     }
-    //     truck[i].cnt = cnt+orderCnt;
-    //     truck[i].d = d;
-    //     truck[i].r = r;
-    //     truck[i].c = c;
-    //     truck[i].bick = bick;
-    //     result["truck_id"] = i;
-    //     result["command"] = order;
-    //     orderList.push(result);
-    // }
+    for(let i=0; i<5; i++){
+        let r = truck[i].r;
+        let c = truck[i].c;
+        let d = truck[i].d;
+        let cnt = truck[i].cnt;
+        let bick = truck[i].bick;
+        let orderCnt = 0;
+        let order = [];
+        let result = {};
+        while(orderCnt<10){
+            if((orderCnt+cnt)%7 === 0){
+                d = (d+1)%4;
+            }
+            let nr = r + dr[d];
+            let nc = c + dc[d];
+            if(nr<0||nc<0||nr>=5||nc>=5){
+                d = (d+2)%4;
+                continue;
+            }
+            if(map[r][c]>3){
+                let want = map[r][c] - 4;
+                while(want!==0&&orderCnt<10&&bick<20){
+                    want--;
+                    orderCnt++;
+                    bick++;
+                    order.push(5);
+                    map[r][c]--;
+                }
+            }
+            else if(map[r][c]<2){
+                let want = 4 - map[r][c];
+                while(want!==0&&orderCnt<10&&bick>0){
+                    want--;
+                    orderCnt++;
+                    bick--;
+                    order.push(6);
+                    map[r][c]++;
+                }
+            }
+            if(orderCnt>=10)    break;
+            r = nr;
+            c = nc;
+            go[r][c]++;
+            order.push(d+1);
+            orderCnt++;
+        }
+        truck[i].cnt = cnt+orderCnt;
+        truck[i].d = d;
+        truck[i].r = r;
+        truck[i].c = c;
+        truck[i].bick = bick;
+        result["truck_id"] = i;
+        result["command"] = order;
+        orderList.push(result);
+    }
     const status = await putSimulate(auth_key, orderList);
     console.log(status, fuck);
     fuck++;
